a22e45f47b68636b3685d0683eddbe0893615ce1
Huy Tran <51260024+huypad@users.noreply.github.com>
2026-07-09 11:25:27 +0700

    Push code

---
+++ JeeBeginner-BE/JeeBeginner/src/app/pages/pages-routing.module.ts
@@ -0,0 +1,56 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './_layout/layout.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'builder',
+        loadChildren: () => import('./builder/builder.module').then((m) => m.BuilderModule),
+      },
+      {
+        path: 'dashboard',
+        loadChildren: () =>
+          import('./JeeBeginner/page-girdters-dashboard/page-girdters-dashboard.module').then((m) => m.PageGirdtersDashboardModule),
+      },
+      {
+        path: 'Management/CustomerManagement',
+        loadChildren: () =>
+          import('./JeeBeginner/Management/CustomerManagement/customer-management.module').then((m) => m.CustomerManagementModule),
+      },
+      {
+        path: 'Management/PartnerManagement',
+        loadChildren: () =>
+          import('./JeeBeginner/Management/PartnerManagement/partner-management.module').then((m) => m.PartnerManagementModule),
+      },
+      {
+        path: 'Management/AccountManagement',
+        loadChildren: () =>
+          import('./JeeBeginner/Management/AccountManagement/account-management.module').then((m) => m.AccountManagementModule),
+      },
+      {
+        path: 'Abc',
+        loadChildren: () =>
+          import('./JeeBeginner/Management/AccountManagement/account-management.module').then((m) => m.AccountManagementModule),
+      },
+      {
+        path: '',
+        redirectTo: '/Management/CustomerManagement',
+        pathMatch: 'full',
+      },
+      {
+        path: '**',
+        redirectTo: 'error/404',
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class PagesRoutingModule { }
---
+++ JeeBeginner-BE/JeeBeginner/src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  appVersion: 'v717demo1',
+  USERDATA_KEY: 'authf649fc9a5f55',
+  isMockEnabled: true,
+  apiUrl: 'api',
+  ApiRoot: 'https://localhost:1404/api',
+  ApiJeeAccount: 'https://localhost:5001/api', //P@sswordJ33Beginn3r
+};
